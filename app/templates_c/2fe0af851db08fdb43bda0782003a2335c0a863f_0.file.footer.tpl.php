b47425d3d448a8ef604e4d6d9e362244073fa98d
Marion Craipeau <marion_craipeau@hotmail.fr>
2016-06-10 14:58:43 +0200

    putain de cache

---
+++ app/templates_c/2fe0af851db08fdb43bda0782003a2335c0a863f_0.file.footer.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-06-10 12:52:59
+  from "C:\wamp64\www\Apply_synth\app\templates\footer.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_575ab82b70db27_42524280',
+  'file_dependency' => 
+  array (
+    '2fe0af851db08fdb43bda0782003a2335c0a863f' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Apply_synth\\app\\templates\\footer.tpl',
+      1 => 1464952255,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_575ab82b70db27_42524280 ($_smarty_tpl) {
+?>
+</BODY>
+</HTML>
+<?php }
+}
