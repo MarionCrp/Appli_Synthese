3beed4d2c783d256d3b2b74aba91f0aa088cfbbd
Marion Craipeau <marion_craipeau@hotmail.fr>
2016-06-10 10:46:06 +0200

    variables pour l'affichage des matchs

--- app/controllers/ajout_match_championnat_controller.php
+++ app/controllers/ajout_match_championnat_controller.php
@@ -14,27 +14,62 @@ if(isset($_POST['saison']) && isset($_POST['pays']) && isset($_POST['division'])
 	$smarty->assign("division", $division);
 	$smarty->display('ajout_match_championnat.tpl');
 
-		if(isset($_SESSION['saison']) && isset($_SESSION['pays']) && isset($_SESSION['division'])){
+	if(isset($_SESSION['saison']) && isset($_SESSION['pays']) && isset($_SESSION['division'])){
 
-			$saison = htmlspecialchars($_SESSION['saison']);
-			$pays = htmlspecialchars($_SESSION['pays']);
-			$division = htmlspecialchars($_SESSION['division']);
+		$saison = htmlspecialchars($_SESSION['saison']);
+		$pays = htmlspecialchars($_SESSION['pays']);
+		$division = htmlspecialchars($_SESSION['division']);
 
-			$champ = $championnat_manager->getChampionnat2($saison, $pays, $division);
+		$champ = $championnat_manager->getChampionnat2($saison, $pays, $division);
+
+
+		$message ="";
+		if(isset($_POST['generer_match_aleat'])){
+			$random->generer_calendrier($champ);
+		}
+		else if (isset($_POST['generer_score'])){
+			$random->generer_
+			score_aleatoire($champ);
+		}
+		else if(isset($_POST['afficher_calendrier'])){
 			$matchs = $match_championnat_manager->getAllMatchChampionnats($champ->id_championnat());
-			var_dump(sizeOf($matchs));
-			$message ="toto";
-			if(isset($_POST['generer_match_aleat'])){
-				$random->generer_calendrier($champ);
-				$message = "Génération du calendrier :";
-			}
-			else if (isset($_POST['generer_score'])){
-				$random->generer_score_aleatoire($champ);
+			$liste_matchs = [];
+			if(sizeOf($matchs) == 0){
+				$message = "Aucun match prévus";
+			} else {
+				foreach($matchs as $match){
+					$liste_matchs[] = array(
+						'id_match_championnat' => $match->id_match_championnat(),
+						'id_equipe_visiteur' => $match->id_equipe_visiteur(),
+						'id_equipe_domicile' => $match->id_equipe_domicile(),
+						'date_match_championnat' => $match->date_match_championnat(),
+						'buts_equipe_visiteur' => $match->buts_equipe_visiteur(),
+						'buts_equipe_domicile' => $match->buts_equipe_domicile(),
+						'arbitre1' => $arbitre_manager->getArbitre($match->id_arbitre1())->nom().' '.prenom();
+						'arbitre2' => $arbitre_manager->getArbitre($match->id_arbitre2())->nom().' '.prenom();
+						'arbitre3' => $arbitre_manager->getArbitre($match->id_arbitre3())->nom().' '.prenom();
+						'arbitre4' => $arbitre_manager->getArbitre($match->id_arbitre4())->nom().' '.prenom();
+						'remplacant' => $arbitre_manager->getArbitre($match->id_remplacant())->nom().' '.prenom();
+						);
+				}
+				$message = "Liste des Matchs";
 			}
 			$smarty->assign("message", $message);
+			$smarty->assign("matchs", $matchs);
 			$smarty->display('matchs_championnat.tpl');
+		} 
+		else if(isset($_POST['afficher_classement'])){
+			$matchs = $match_championnat_manager->getAllMatchChampionnats($champ->id_championnat());
+			if(sizeOf($matchs) == 0){
+				$message = "Aucun classement";
+			} else {
+				$message = "Classement";
+			}
+			$smarty->assign("message", $message);
+			$smarty->assign("matchs", $matchs);
+			$smarty->display('classement.tpl');
 		}
-	
-} else {
+	} else {
 	header('Location: selection_championnat_controller.php');
+	}
 }
--- app/templates_c/0d71789b622fe0f4edb86e8566774ef9778ec6b3_0.file.matchs_championnat.tpl.cache.php
+++ app/templates_c/0d71789b622fe0f4edb86e8566774ef9778ec6b3_0.file.matchs_championnat.tpl.cache.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-06-10 07:55:35
+/* Smarty version 3.1.29, created on 2016-06-10 08:08:44
   from "C:\wamp64\www\Apply_synth\app\templates\matchs_championnat.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_575a72773ac2c7_71357685',
+  'unifunc' => 'content_575a758c1b2851_79532075',
   'file_dependency' => 
   array (
     '0d71789b622fe0f4edb86e8566774ef9778ec6b3' => 
     array (
       0 => 'C:\\wamp64\\www\\Apply_synth\\app\\templates\\matchs_championnat.tpl',
-      1 => 1465545321,
+      1 => 1465545689,
       2 => 'file',
     ),
   ),
@@ -22,8 +22,8 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_575a72773ac2c7_71357685 ($_smarty_tpl) {
-$_smarty_tpl->compiled->nocache_hash = '29407575a72771c2af2_72812003';
+function content_575a758c1b2851_79532075 ($_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '4951575a758c0d71e3_24110636';
 $_smarty_tpl->smarty->ext->_subtemplate->render($_smarty_tpl, "file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 9999, $_smarty_tpl->cache_lifetime, array('title'=>'foo'), 0, false);
 ?>
 
